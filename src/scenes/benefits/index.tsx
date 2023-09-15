f722e14cecc2086ab9d1b8b8590487c7f5663d04
ZainUlAbedin21 <zain.ul.abdin2121@gmail.com>
2023-09-15 13:20:23 +0500

    Start working on the Our Portion

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -3,7 +3,7 @@ import { BenefitType, SelectedPage } from "@/shared/types"
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
 import { motion } from "framer-motion"
 import Benefit from "./Benefit"
-import BenefitsPageGraphic from "@/assets/Be"
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png"
 import ActionButton from "@/shared/ActionButton"
 
 const benefits:Array<BenefitType>=[
@@ -79,7 +79,7 @@ variants={container}
 </motion.div>
 
 {/* GRAPHICS AND DESCRIPTION*/}
-<div>
+<div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
     {/* GRAPHIC */}
 <img
 className="mx-auto"
@@ -91,23 +91,41 @@ src={BenefitsPageGraphic}
 {/**TITLE */}
 <div className="relative">
     <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
-    <div>
+    <motion.div className="md:my-5 md:w-3/5"
+initial="hidden"
+whileInView="visible"
+viewport={{once:true, amount:0.5}}
+transition={{delay:0.2, duration:0.5}}
+variants={{
+  hidden:{opacity:0,x:50},
+  visible:{opacity:1,x:0}
+}}
+>
         <HText>
             MILLIONS OF HAPPY MEMBERS GETTING{" "}
             <span className="text-primary-500">FIT</span>
         </HText>
-    </div>
+    </motion.div>
     </div>
 
 </div>
 {/**DESCRIPT */}
-
+<motion.div 
+initial="hidden"
+whileInView="visible"
+viewport={{once:true, amount:0.5}}
+transition={{delay:0.2, duration:0.5}}
+variants={{
+  hidden:{opacity:0,x:50},
+  visible:{opacity:1,x:0}
+}}
+>
 <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet non voluptatem doloremque maxime nostrum. Placeat, hic veritatis. Dolorem tempora, consectetur dicta provident minima numquam nemo velit consequuntur! Animi, asperiores voluptatem!
    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet non voluptatem doloremque maxime nostrum. Placeat, hic veritatis. Dolorem tempora, consectetur dicta provident minima numquam nemo velit consequuntur! Animi, asperiores voluptatem!</p>
 <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt culpa quos rem? Quam accusamus aut, temporibus pariatur corporis recusandae quas hic doloremque quod expedita consequuntur sit, sint quasi alias aliquam?
    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet non voluptatem doloremque maxime nostrum. Placeat, hic veritatis. Dolorem tempora, consectetur dicta provident minima numquam nemo velit consequuntur! Animi, asperiores voluptatem!
 </p>
-
+</motion.div>
 {/**BUTTON */}
 <div className="relative mt-16">
     <div className="before:absolute before:-bottom-20 before:right-40 before:content-sparkles">
