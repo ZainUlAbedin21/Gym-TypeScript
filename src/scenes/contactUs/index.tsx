4486442973f89cb3288e1a0f418040c52c67e992
ZainUlAbedin21 <zain.ul.abdin2121@gmail.com>
2023-09-19 08:44:14 +0500

    Working On Contact Us

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,15 +1,18 @@
 import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import { Form } from "react-hook-form";
-
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png"
 
 type Props = {
     setSelectedPage:(value:SelectedPage)=> void;
 };
 const ContactUs = ({setSelectedPage}: Props) => {
   return (
-    <div>ContactUs</div>
-  )
+<section id="contactus" className="mx-auto w-5/6 pt-24 pb-22 ">
+<motion.div onViewportEnter={()=>setSelectedPage(SelectedPage.ContactUs)}>
+
+</motion.div>
+</section>  )
 }
 
 export default ContactUs
