f33a4eb77d7ff27f5278307121008d1c79e641c2
ZainUlAbedin21 <zain.ul.abdin2121@gmail.com>
2023-09-19 08:27:05 +0500

    Completed Our Classes Portion . Then start working on Contact Us

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,15 @@
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { Form } from "react-hook-form";
+
+
+type Props = {
+    setSelectedPage:(value:SelectedPage)=> void;
+};
+const ContactUs = ({setSelectedPage}: Props) => {
+  return (
+    <div>ContactUs</div>
+  )
+}
+
+export default ContactUs
