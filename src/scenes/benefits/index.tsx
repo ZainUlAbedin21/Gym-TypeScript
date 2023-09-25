cf497282bafbaac717373703f5443f549feed7ca
ZainUlAbedin21 <zain.ul.abdin2121@gmail.com>
2023-09-25 07:21:04 +0500

    NEW CHANGES

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,147 +1,165 @@
-import HText from "@/shared/HText"
-import { BenefitType, SelectedPage } from "@/shared/types"
-import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
-import { motion } from "framer-motion"
-import Benefit from "./Benefit"
-import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png"
-import ActionButton from "@/shared/ActionButton"
+import ActionButton from "@/shared/ActionButton";
+import HText from "@/shared/HText";
+import { BenefitType, SelectedPage } from "@/shared/types";
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
+import NewBenefits from "@/assets/friendly-man.png";
+import Benefit from "./Benefit";
 
-const benefits:Array<BenefitType>=[
-    {
-        icon:<HomeModernIcon className="h-6 w-6"/>,
-        title:"State of The Art Facilities",
-        description:"State of The Art Facilities State of The Art Facilities State of The Art Facilities State of The Art Facilities State of The Art Facilities State of The Art Facilities"
-    },
-    {
-        icon:<UserGroupIcon className="h-6 w-6"/>,
-        title:"2 of The Art Facilities",
-        description:"State of The Art Facilities State of The Art Facilities State of The Art Facilities State of The Art Facilities State of The Art Facilities State of The Art Facilities"
-    },
-    {
-        icon:<AcademicCapIcon className="h-6 w-6"/>,
-        title:"3 of The Art Facilities",
-        description:"State of The Art Facilities State of The Art Facilities State of The Art Facilities State of The Art Facilities State of The Art Facilities State of The Art Facilities"
-    },
-    
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+  },
 ];
 
-const container={
-    hidden:{},
-    visible:{
-        transition:{staggerChildren:0.2}
-    }
-}
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
 
-HomeModernIcon
 type Props = {
-    setSelectedPage:(value: SelectedPage)=>void;
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Benefits = ({setSelectedPage}: Props) => {
+const Benefits = ({ setSelectedPage }: Props) => {
   return (
-<section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
-<motion.div onViewportEnter={()=> setSelectedPage(SelectedPage.Benefits)}>
-    {/*HEADER */}
-<motion.div className="md:my-5 md:w-3/5"
-initial="hidden"
-whileInView="visible"
-viewport={{once:true, amount:0.5}}
-transition={{delay:0.2, duration:0.5}}
-variants={{
-  hidden:{opacity:0,x:-50},
-  visible:{opacity:1,x:0}
-}}
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        {/* HEADER */}
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>MORE THAN JUST GYM.</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </motion.div>
 
+        {/* BENEFITS */}
+        <motion.div
+          className="mt-5 items-center justify-between gap-8 md:flex"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
 
->
-    <HText>MORE THAN JUST GYM.</HText>
-    <p className="my-5 text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum doloremque illo unde accusamus obcaecati! Repellendus iusto, nam similique blanditiis, nesciunt, omnis illum labore natus velit iste quam voluptatem magnam illo!</p>
-</motion.div>
-{/*BENEFITS */}
-<motion.div className="md-flex items-center justify-between gap-8 mt-5"
-initial="hidden"
-whileInView="visible"
-viewport={{once:true,amount:0.5}}
-variants={container}
->
+        {/* GRAPHICS AND DESCRIPTION */}
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          {/* GRAPHIC */}
+          <img
+            className="mx-auto"
+            alt="benefits-page-graphic"
+            src={NewBenefits}
+          />
 
-{benefits.map((benefit:BenefitType)=>(
-    <Benefit
-    key={benefit.title}
-    icon={benefit.icon}
-    title={benefit.title}
-    description={benefit.description}
-    setSelectedPage={setSelectedPage}
-    />
-))}
+          {/* DESCRIPTION */}
+          <div>
+            {/* TITLE */}
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <HText>
+                    MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>
+                  </HText>
+                </motion.div>
+              </div>
+            </div>
 
+            {/* DESCRIPT */}
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className="my-5">
+                Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                Felis orci diam odio.
+              </p>
+              <p className="mb-5">
+                Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                enim mattis odio in risus nunc.
+              </p>
+            </motion.div>
 
-</motion.div>
+            {/* BUTTON */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
-{/* GRAPHICS AND DESCRIPTION*/}
-<div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
-    {/* GRAPHIC */}
-<img
-className="mx-auto"
-alt="benefits-page-graphic"
-src={BenefitsPageGraphic}
-/>
-{/*DESCRIPTION */}
-<div>
-{/**TITLE */}
-<div className="relative">
-    <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
-    <motion.div className="md:my-5 md:w-3/5"
-initial="hidden"
-whileInView="visible"
-viewport={{once:true, amount:0.5}}
-transition={{delay:0.2, duration:0.5}}
-variants={{
-  hidden:{opacity:0,x:50},
-  visible:{opacity:1,x:0}
-}}
->
-        <HText>
-            MILLIONS OF HAPPY MEMBERS GETTING{" "}
-            <span className="text-primary-500">FIT</span>
-        </HText>
-    </motion.div>
-    </div>
-
-</div>
-{/**DESCRIPT */}
-<motion.div 
-initial="hidden"
-whileInView="visible"
-viewport={{once:true, amount:0.5}}
-transition={{delay:0.2, duration:0.5}}
-variants={{
-  hidden:{opacity:0,x:50},
-  visible:{opacity:1,x:0}
-}}
->
-<p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet non voluptatem doloremque maxime nostrum. Placeat, hic veritatis. Dolorem tempora, consectetur dicta provident minima numquam nemo velit consequuntur! Animi, asperiores voluptatem!
-   Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet non voluptatem doloremque maxime nostrum. Placeat, hic veritatis. Dolorem tempora, consectetur dicta provident minima numquam nemo velit consequuntur! Animi, asperiores voluptatem!</p>
-<p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt culpa quos rem? Quam accusamus aut, temporibus pariatur corporis recusandae quas hic doloremque quod expedita consequuntur sit, sint quasi alias aliquam?
-   Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet non voluptatem doloremque maxime nostrum. Placeat, hic veritatis. Dolorem tempora, consectetur dicta provident minima numquam nemo velit consequuntur! Animi, asperiores voluptatem!
-</p>
-</motion.div>
-{/**BUTTON */}
-<div className="relative mt-16">
-    <div className="before:absolute before:-bottom-20 before:right-40 before:content-sparkles">
-
-    <ActionButton setSelectedPage={setSelectedPage}>
-    Join Now
-    </ActionButton>
-
-    </div>
-
-</div>
-</div>
-</div>
-
-</motion.div>
-</section>  )
-}
-
-export default Benefits
+export default Benefits;
